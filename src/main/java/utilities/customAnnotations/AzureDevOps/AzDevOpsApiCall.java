4bc2bfc97ee70b61ddba0fb622adc81a6bf30c30
bhargavkumare.65 <bhargavkumare.65>
2021-08-18 22:47:43 +0530

    azdevops listener

---
+++ src/main/java/utilities/customAnnotations/AzureDevOps/AzDevOpsApiCall.java
@@ -0,0 +1,29 @@
+package utilities.customAnnotations.AzureDevOps;
+
+import io.restassured.response.Response;
+import lombok.SneakyThrows;
+
+import static io.restassured.RestAssured.given;
+
+public class AzDevOpsApiCall {
+
+    private static final String ISSUE_TRACKER_API_BASE_URL = "URL";
+    private static final String Username = "USERNAME";
+    public static final String Token = "PWD";
+
+    @SneakyThrows
+    public static String getWorkItemStatus(String IssueID) {
+
+            Response response = given()
+                    .header("Accept", "application/json")
+                    .queryParam("api-version","6.1-preview.3")
+                    .auth().preemptive().basic(Username, Token)
+                    .baseUri(ISSUE_TRACKER_API_BASE_URL)
+                    .when()
+                    .get(IssueID+"?api-version=6.1-preview.3")
+                    .then()
+                    .statusCode(200).extract().response();
+        return response.jsonPath().getString("fields.'System.State'");
+    }
+
+}
---
+++ src/main/java/utilities/customAnnotations/AzureDevOps/AzIssueStatusListener.java
@@ -0,0 +1,41 @@
+package utilities.customAnnotations.AzureDevOps;
+
+import org.testng.IInvokedMethod;
+import org.testng.IInvokedMethodListener;
+import org.testng.ITestResult;
+
+
+public class AzIssueStatusListener  implements IInvokedMethodListener {
+    public static final String BACKLOG ="Backlog";
+    public static final String DEVELOPMENT_STARTED= "Development Started";
+    public static final String str_Code_Review= "Code Review";
+    public static final String str_DEVELOPMENT_IN_PROGRESS="Development In Progress";
+    public static final String str_READY_TO_TEST ="Ready To Test";
+    public static final String TESTING_IN_PROGRESS ="Test In Progress";
+    public static final String DONE ="DONE";
+
+    @Override
+    public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
+
+        AzureDevOpsIssue issueid = method.getTestMethod()
+                .getConstructorOrMethod()
+                .getMethod()
+                .getAnnotation(AzureDevOpsIssue.class);
+
+        if (null != issueid) {
+            if(str_READY_TO_TEST.equalsIgnoreCase(AzDevOpsApiCall.getWorkItemStatus(issueid.AzIssueID()))){
+                switch(testResult.getStatus()){
+                    case ITestResult.FAILURE:
+                        // no need to fail as we might have expected this already.
+                        testResult.setStatus(ITestResult.SKIP);
+                        break;
+                    case ITestResult.SUCCESS:
+                        // TO-DO Write Code to Move the AZ Ticket from Ready to Test to Tested/Done
+                        break;
+                }
+            }
+        }
+    }
+
+
+}
